276e989732b9ae0563abd51a3aa59d2f59e220ff
Sören Tempel <soeren+git@soeren-tempel.net>
2020-03-08 15:52:26 +0100

    Store parser test input in files

--- tests.scm
+++ tests.scm
@@ -1,10 +1,14 @@
 (load "parser.scm")
 (import comparse test)
 
+(define (parse-file path)
+  (call-with-input-file (string-append "testdata" "/" path)
+    (lambda (port) (parse parse-entry port))))
+
 (test-group "parser"
   (test "parse entry without fields and notes"
     (list (make-meta #\-
                "thompson1984trust"
                "Ken Thompson"
                "Reflections on Trusting Trust") '() '())
-    (parse parse-entry "- <thompson1984trust> Ken Thompson: Reflections on Trusting Trust\n")))
+    (parse-file "thompson1984trust.txt")))
