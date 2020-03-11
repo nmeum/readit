4ffb8de3601d813bb8ac2d8ed12f66d7981c6ab7
Sören Tempel <soeren+git@soeren-tempel.net>
2020-03-11 18:16:23 +0100

    readit.scm: Parse files and print resulting object

--- readit.scm
+++ readit.scm
@@ -1,4 +1,5 @@
-(import (chicken process-context) srfi-37)
+(import (chicken process-context) (chicken file)
+        (readit parser) srfi-1 srfi-37)
 
 (define names '())
 
@@ -19,6 +20,17 @@
   (print "Usage: readit [-n NAME] FILE...")
   (exit))
 
+(define (parse-file path)
+  (call-with-input-file path
+    (lambda (port)
+      (let ((r (parse-readit port)))
+        (if r r (error "syntax error in" fp))))))
+
+(define (parse-files fps)
+  (fold (lambda (fp entries)
+          (append (parse-file fp) entries))
+        '() fps))
+
 (define (parse-args)
   (args-fold
     (command-line-arguments)
@@ -29,11 +41,15 @@
     '()))
 
 (define (main)
-  (let ((files (parse-args)))
-    (if (null? files)
-      (usage)
-      (begin
-        (print names)))))
+  (let ((fps (parse-args)))
+    (when (null? fps)
+      (usage))
+
+    (for-each (lambda (fp)
+                (unless (file-exists? fp)
+                  (error "file does not exist" file))) fps)
+
+    (print (parse-files fps))))
 
 (cond-expand
   ((or chicken-script compiling) (main))
